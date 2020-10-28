9e30912da73e6b16e74a5b05ce78aa24ea9cf371
jcvidela <juancarlos.videla00@gmail.com>
2020-10-28 20:39:52 -0300

    refactoring the project structure

--- App.js
+++ App.js
@@ -1,41 +1,13 @@
 import React from "react";
-import { createStore, combineReducers } from "redux";
 import { Provider } from "react-redux";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import store from './src/store';
 
-const reducers = {
-  todos: (state = [], action) => state,
-};
-
-const MyApp = () => {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-};
-
-const store = createStore(
-  combineReducers({
-    ...reducers,
-  })
-);
+import App from './src/index';
 
 export default () => {
   return (
     <Provider store={store}>
-      <MyApp />
+      <App />
     </Provider>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
