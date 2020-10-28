62046000e8d083d63e2c0b64dd6db0098ed01a7e
jcvidela <juancarlos.videla00@gmail.com>
2020-10-28 20:21:42 -0300

    adding redux, initial config

--- App.js
+++ App.js
@@ -1,27 +1,41 @@
-import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+const reducers = {
+  todos: (state = [], action) => state,
+};
 
 const MyApp = () => {
-  return(
+  return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
-  )
+  );
 };
+
+const store = createStore(
+  combineReducers({
+    ...reducers,
+  })
+);
 
 export default () => {
   return (
-    <MyApp />
+    <Provider store={store}>
+      <MyApp />
+    </Provider>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
